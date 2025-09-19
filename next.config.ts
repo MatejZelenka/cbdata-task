52072dbc005135f424f072c27459643855e5e8b9
Matěj Zelenka <matej.zelenka@tfbank.se>
2025-09-19 10:24:13 +0200

    Initial config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'],
+  },
+  // turbopack: {
+  //   root: './pnpm-lock.yaml',
+  // },
 };
 
 export default nextConfig;
